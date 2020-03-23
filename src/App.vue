d9a57257b615685fdd0c03079d2f6ef2b3ccf910
ChoSaJang <enjoy.hyun@gmail.com>
2020-03-23 12:18:43 +0900

    - view/router 변경 샘플

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HomeView/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HomeView from './components/HomeView'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
+    HomeView
   },
 
   data: () => ({
